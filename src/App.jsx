20d870a732103a668043fc1606f2369320c42b66
Yash Sahu <sahuyash359@gmail.com>
2026-08-04 15:58:26 +0530

    Multiple conditional in React

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import { useState } from 'react';
 import User from './User';
 
 function App() {
-  const [display,setDisplay] = useState(true);
+  const [count,setCount] = useState(0);
   
   return (
     <>
-    <button onClick={()=>setDisplay(!display)}>Toggle</button>
+    <h1>{count}</h1>
+    <button onClick={()=>setCount(count+1)}>Click</button>
       {
-        display?<User/>:null
+        count === 0?<h1>Condition 0</h1>
+        :count === 1?<h1>Condition 1</h1>
+        :count === 2?<h1>Condition 2</h1>
+        :count === 3?<h1>Condition 3</h1>
+        :count === 4?<h1>Condition 4</h1>
+        :count === 5?<h1>Condition 5</h1>
+        :null
       }
     </>
   )
