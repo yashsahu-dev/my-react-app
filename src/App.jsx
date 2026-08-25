17a8f57744f48dbb665a7b10b39605170f59aa2e
Yash Sahu <sahuyash359@gmail.com>
2026-08-25 16:14:37 +0530

    Useref hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import React, {  useState } from 'react'
-import Counter from './Counter';
-
+import React from 'react'
+import { useRef } from 'react'
 const App = () => {
 
-  const [Count, setCount] = useState(0);
-  const [Data, setData] = useState(0);
-  const [display,setDislay] = useState(true);
-  
-
-  
-
+  const inputRef =  useRef(null)
+  const h1Ref =  useRef(null)
+  const inpHandler=()=>{
+    inputRef.current.focus();
+    inputRef.current.style.color = "blue"
+  }
+  const h1Handler=()=>{
+    h1Ref.current.style.color = "red"
+  }
   return (
     <div>
-      {
-        display?<Counter count={Count} data={Data} />:null
-      }
-      <button onClick={()=>setCount(Count+1)}>Counter</button>
-      <button onClick={()=>setData(Data+1)}>Data</button>
-      <button onClick={()=>setDislay(!display)}>Toggle</button>
+      <input ref={inputRef} type="text" />
+      <button onClick={inpHandler}>Click</button>
+      <h1 ref={h1Ref}>Hello</h1>
+      <button onClick={h1Handler}>Click</button>
     </div>
   )
 }
