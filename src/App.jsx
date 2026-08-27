81904182b0c36397f2054287d45ff87f63cb6aa9
Yash Sahu <sahuyash359@gmail.com>
2026-08-27 19:40:11 +0530

    useTransition hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
-import React from "react";
-import { useFormStatus } from "react-dom";
+import React, { useTransition } from 'react'
 
 const App = () => {
-  const handleForm = async () => {
-    await new Promise((res) => setTimeout(res, 5000));
-    console.log("Submitted!!!");
-  };
-  const Form = () => {
-    const {pending} = useFormStatus();
-    console.log(pending)
-    return (
-      <div>
-          <input type="text" placeholder="Enter username" />
-          <input type="text" placeholder="Enter password" />
-          <button type="submit" disabled={pending}>{pending?"submitting...":"submit"}</button>
-        
-      </div>
-    );
-  };
-  return <div>
-    <form action={handleForm}>
-      <Form/>
-    </form>
-  </div>;
-};
+  const [pending,Transition] = useTransition();
+  const handleBtn=()=>{
+    Transition(async ()=>{
+       await new Promise(res=>setTimeout(res,6000))
+    })
+  }
+  return (
+    <div>
+      <input type="text" /><br />
 
-export default App;
+      {pending?<img style={{width:"100px"}} src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbTZvYnFuNWZsYWhqd2M2OG85aDRiejFjd2FjdXdrMTU1a2owZXdxOCZlcD12MV9naWZzX3JlbGF0ZWQmY3Q9Zw/3oEjI6SIIHBdRxXI40/giphy.gif" alt="" />:null} <br />
+      <button type="submit" onClick={handleBtn} disabled={pending} >{pending?"Submitting...":"submit"}</button>
+    </div>
+  )
+}
+
+export default App
