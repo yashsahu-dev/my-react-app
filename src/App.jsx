b6c6dd5b49af7d61463f998671ab5a2b6aca377c
Yash Sahu <sahuyash359@gmail.com>
2026-08-28 19:06:11 +0530

    Lifting state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 import React, { useState } from 'react'
+import AddUser from './AddUser';
+import DisplayUser from './DisplayUser';
 
 const App = () => {
-  const [User, setUser] = useState("");
-  const [Users, setUsers] = useState([]);
-  const HandleUser=()=>{
-    setUsers([...Users,User]);
-  }
-
-  const Total = Users.length;
-  const last = Users[Users.length-1];
-  const unique = [...new Set(Users)].length
+  const [user, setUser] = useState('');
   return (
     <div>
-      <h2>Total users: {Total}</h2>
-      <h2>Last user: {last}</h2>
-      <h2>Unique users: {unique}</h2>
-      <input type="text" onChange={(e)=>setUser(e.target.value)} /><br />
-      <button onClick={HandleUser}>Add User</button>
-      {
-        Users.map((item)=>(
-          <h4>{item}</h4>
-        ))
-      }
+      <AddUser  setUser={setUser}/>
+      <DisplayUser user={user}/>
     </div>
   )
 }
---
+++ src/DisplayUser.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DisplayUser = ({user}) => {
+  return (
+    <div>
+        <h2>Display User</h2>
+        <h2>{user}</h2>
+    </div>
+  )
+}
+
+export default DisplayUser
