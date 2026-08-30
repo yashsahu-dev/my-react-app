09e730978f5c1528f2ec68c56d4ec8604eda4951
Yash Sahu <sahuyash359@gmail.com>
2026-08-30 11:29:34 +0530

    simple form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
-import { NavLink, Route, Routes } from "react-router";
-import "./App.css";
-import UserList from "./UserList";
-import UserAdd from "./UserAdd";
-import UserEdit from "./UserEdit";
+import React, { useState } from 'react'
+import './App.css'
 const App = () => {
-  return <div>
-    <ul>
-      <li>
-        <NavLink to='/'>Home</NavLink>
-      </li>
-      <li>
-        <NavLink to='/adduser'>Add Users</NavLink>
-      </li>
-    </ul>
-    <Routes>
-      <Route path="/" element={<UserList/>}/>
-      <Route path="/adduser" element={<UserAdd/>}/>
-      <Route path="/edituser/:id" element={<UserEdit/>}/>
-    </Routes>
-  </div>;
-};
+  const [name, setName] = useState('');
+  const [nameerr, setNameErr] = useState();
 
-export default App;
+  const [pass, setPass] = useState('');
+  const [passerr, setPassErr] = useState();
+
+  const handleName=(e)=>{
+    if(e.target.value.length>5){
+      setNameErr('only 5 chars allowed!!!');
+    }
+    else{
+      setNameErr('')
+    }
+  }
+  const handlePass=(e)=>{
+    let regex = /^[A-Z0-9]+$/i
+    if(!regex.test(e.target.value)){
+      setPassErr('only alphanumeric chars allowed!!!');
+    }
+    else{
+      setPassErr('')
+    }
+  }
+  return (
+    <div>
+      <input type="text" className={nameerr?'error':''} onChange={handleName} placeholder='Enter name' /><span>{nameerr}</span><br /><br />
+      <input type="text" className={passerr?'error':''} onChange={handlePass} placeholder='Enter password' /><span>{passerr}</span><br /><br />
+      
+      <button disabled={nameerr || passerr}>Login</button>
+    </div>
+  )
+}
+
+export default App
